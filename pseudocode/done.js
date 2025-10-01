0414802393d45bad72cde71aeed38772545f765c
Jacob1823 <jacob114244@student-schoolsofwestfield.org>
2025-10-01 13:54:59 +0000

    guess

--- pseudocode/done.js
+++ pseudocode/done.js
@@ -1,1 +1,22 @@
+  function guessGame(){
+
+    let answer = Math.floor(Math.random()*100+1);
+
+    let guess = 0
+
+    while (guess != answer || guess != "q"){
+
+      guess = prompt("Guess a number, 1-100");
+
+      if (guess == q) alert("Quitter!");
+
+      if (guess == answer) alert("Correct!");
+
+      else if (guess > answer) alert("Too high!");
+
+      else if (guess < answer) alert("Too low!");
+
+      else alert("Bad input");
+
+    }
 
